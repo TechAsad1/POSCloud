ae1fa8bc093f5031292eb6d9bcd8c284b1b61f9a
TechAsad1 <techinfo517@gmail.com>
2025-07-23 03:41:06 +0500

    Change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import { base_path } from "./environment.jsx";
-import "../src/style/css/feather.css";
-import "../src/style/css/line-awesome.min.css";
-import "../src/style/scss/main.scss";
-import "../src/style/icons/fontawesome/css/fontawesome.min.css";
-import "../src/style/icons/fontawesome/css/all.min.css";
+import "./style/css/feather.css";
+import "./style/css/line-awesome.min.css";
+import "./style/scss/main.scss";
+import "./style/icons/fontawesome/css/fontawesome.min.css";
+import "./style/icons/fontawesome/css/all.min.css";
 import { createRoot } from "react-dom/client";
 
 import { Provider } from "react-redux";
@@ -15,7 +15,6 @@ import store from "./core/redux/store.jsx";
 import AllRoutes from "./Router/router.jsx";
 
 const rootElement = document.getElementById("root");
-
 if (rootElement) {
   const root = createRoot(rootElement);
   root.render(
