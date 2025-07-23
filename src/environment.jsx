aa0edb99e3ea65c51b3fbf32e3e7a60cb85e7a89
TechAsad1 <techinfo517@gmail.com>
2025-07-23 05:06:04 +0500

    Update

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,5 +1,5 @@
 // export const base_path = "/react/template";
 export const base_path = "/";
-export const image_path ='/react/template/'
+export const image_path ='/assets/img/'
 // export const image_path ='/'
 
